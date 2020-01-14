0fe67fdd1b05e7c7f61149474566d81d80759ec1
John Gaskin <jgaskin@act.md>
2020-01-14 17:44:20 -0500

    Navbar updated

--- gulpfile.js
+++ gulpfile.js
@@ -101,8 +101,10 @@ gulp.task('copy-patterns-css', function() {
  */
 gulp.task('copy-patterns-js', function() {
   return gulp.src([
+    'node_modules/popper.js/dist/popper.js',
     'node_modules/jquery/dist/jquery.min.js',
     'node_modules/bootstrap/dist/js/bootstrap.min.js',
+    'node_modules/bootstrap/dist/js/bootstrap.min.js.map',
     'node_modules/tether/dist/js/tether.min.js'
   ]).pipe(gulp.dest('./out/js'))
 });
